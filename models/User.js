d2efc4c5474d1b7ec5c09c3eb5c298a523ef6b70
Angela Man <angela.k.man@gmail.com>
2021-06-05 08:49:24 -0500

    use bcrypt and hooks to hash password

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create User model
-class User extends Model {
-
-}
+class User extends Model {}
 
 // define table columns and configuration
 User.init(
@@ -44,6 +43,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle hook functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle hook functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // table configuration options
 
         // pass in imported sequelize connection
