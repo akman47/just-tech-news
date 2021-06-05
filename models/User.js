9303625a8fa215bdd6faf9b823e4176bf22595fd
Angela Man <angela.k.man@gmail.com>
2021-06-04 22:34:46 -0500

    create user model with sequelize

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create User model
+class User extends Model {
+
+}
+
+// define table columns and configuration
+User.init(
+    {
+        // table column definitions
+        // define id column
+        id: {
+            // use special Sequelize DataTypes object to provide what type of data it is
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define username column
+        username: {
+            type: DataTypes,stringify,
+            allowNull: false
+        },
+        // define email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // cannot be any duplicate email values in table
+            unique: true,
+            // if allowNull is set to false, we can run data through validators before creating table data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        // table configuration options
+
+        // pass in imported sequelize connection
+        sequelize,
+        // don't automatically create timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
